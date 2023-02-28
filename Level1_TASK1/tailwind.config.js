cf69e4254022267d25ef0cfe61340688025a2449
urstrulynishkarsh <206301010@gkv.ac.in>
2023-02-28 17:11:43 +0530

    first commit

---
+++ Level1_TASK1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend:
+     {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+     },
+  },
+},
+  plugins: [],
+}
